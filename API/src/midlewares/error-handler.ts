a287efbd43c3e7c6eb0250f6054680ea518a5ca6
FelipeAugustoAMF <felipea.mendonca@hotmail.com>
2025-04-23 16:47:01 -0300

    initial commit

---
+++ API/src/midlewares/error-handler.ts
@@ -0,0 +1,8 @@
+export const errorHandler = (err: Error, req: any, res: any, next: any) => {
+    res.status(500).json({
+        "success": false,
+        "message": err.message,
+        "stack": err.stack,
+    });
+    next(err);
+}
