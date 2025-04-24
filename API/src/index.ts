356d317917075b0389835facd08eb830c990543b
FelipeAugustoAMF <felipea.mendonca@hotmail.com>
2025-04-24 14:56:33 -0300

    enabling cors in the backend

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import cors from "cors";
 import { errorHandler } from "./midlewares/error-handler";
 import apiRoutes from "./routes";
 
 const app = express();
 const port = 8080;
+
+// enabling cors so that the frontend can access the backend from any origin
+app.use(cors({
+    origin: '*'
+}));
 
 app.use('/api', apiRoutes);
 
